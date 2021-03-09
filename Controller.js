18755b87e6b2f2e5fecced05cc899bcc67d71337
XGriffers <77297581+XGriffers@users.noreply.github.com>
2021-03-08 21:17:46 -0500

    Add files via upload

---
+++ Controller.js
@@ -0,0 +1,177 @@
+
+function validation(server, database, user, password) {
+  if (server == "" || database == "" || user == "") {
+      alert("Fields Should Not Be Empty!");
+  }
+  else if(password.length <= 6){
+    var num = Number(password);
+    if(!(num > 0)){
+      alert("Your Password must be at least 6 charactors or more");
+    }
+  }
+}
+
+function Connect(server, database, user, password) {
+  var connectionString =
+    "Data Source=" + server + "\\SQLEXPRESSIT350;Initial Catalog=" + database + ";User ID=" + user + ";Password=" + password + ";Provider=SQLOLEDB";
+
+  //alert(connectionString);
+
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+
+  alert("Connected");
+}
+function CusomerCount(server, database, user, password) {
+  var connectionString =
+    "Data Source=" +
+    server +
+    "\\SQLEXPRESSIT350;Initial Catalog=" +
+    database +
+    ";User ID=" +
+    user +
+    ";Password=" +
+    password +
+    ";Provider=SQLOLEDB";
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+
+  var rs = new ActiveXObject("ADODB.Recordset");
+  rs.Open("Select count(*) From Customers", Connection);
+  rs.MoveFirst
+
+  while (!rs.eof) {
+    alert(rs.fields(0));
+    rs.movenext;
+  }
+  rs.close;
+  Connection.close;
+};
+function ShowNames(server, database, user, password) {
+  var connectionString =
+    "Data Source=" +
+    server +
+    "\\SQLEXPRESSIT350;Initial Catalog=" +
+    database +
+    ";User ID=" +
+    user +
+    ";Password=" +
+    password +
+    ";Provider=SQLOLEDB";
+
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+  var rs = new ActiveXObject("ADODB.Recordset");
+  rs.Open("Select CompanyName From Customers ", Connection);
+  rs.MoveFirst
+  var input = " ";
+  while (!rs.eof) {
+    input += rs.fields(0) + "\n";
+    rs.movenext;
+  }
+  alert(input);
+  rs.close;
+  Connection.close;
+};
+function OrderCount(server, database, user, password) {
+  var connectionString =
+    "Data Source=" +
+    server +
+    "\\SQLEXPRESSIT350;Initial Catalog=" +
+    database +
+    ";User ID=" +
+    user +
+    ";Password=" +
+    password +
+    ";Provider=SQLOLEDB";
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+
+  var rs = new ActiveXObject("ADODB.Recordset");
+  rs.Open("Select count(*) From Orders", Connection);
+  rs.MoveFirst
+
+  while (!rs.eof) {
+    alert(rs.fields(0));
+    rs.movenext;
+  }
+  rs.close;
+  Connection.close;
+};
+function OrderShipNames(server, database, user, password) {
+  var connectionString =
+    "Data Source=" +
+    server +
+    "\\SQLEXPRESSIT350;Initial Catalog=" +
+    database +
+    ";User ID=" +
+    user +
+    ";Password=" +
+    password +
+    ";Provider=SQLOLEDB";
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+
+  var rs = new ActiveXObject("ADODB.Recordset");
+  rs.Open("Select ShipName From Orders", Connection);
+  rs.MoveFirst
+  var input = ' ';
+  while (!rs.eof) {
+    input += rs.fields(0) + "\n";
+    rs.movenext;
+  }
+  alert(input);
+  rs.close;
+  Connection.close;
+};
+function EmpNamesCount(server, database, user, password) {
+  var connectionString =
+    "Data Source=" +
+    server +
+    "\\SQLEXPRESSIT350;Initial Catalog=" +
+    database +
+    ";User ID=" +
+    user +
+    ";Password=" +
+    password +
+    ";Provider=SQLOLEDB";
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+
+  var rs = new ActiveXObject("ADODB.Recordset");
+  rs.Open("Select count(*) from Employees", Connection);
+  rs.MoveFirst
+
+  while (!rs.eof) {
+    alert(rs.fields(0));
+    rs.movenext;
+  }
+  rs.close;
+  Connection.close;
+};
+function EmpNames(server, database, user, password) {
+  var connectionString =
+    "Data Source=" +
+    server +
+    "\\SQLEXPRESSIT350;Initial Catalog=" +
+    database +
+    ";User ID=" +
+    user +
+    ";Password=" +
+    password +
+    ";Provider=SQLOLEDB";
+  var Connection = new ActiveXObject("ADODB.Connection");
+  Connection.Open(connectionString);
+
+  var rs = new ActiveXObject("ADODB.Recordset");
+  rs.Open("Select FirstName + ' ' + LastName from Employees Order by LastName asc", Connection);
+  rs.MoveFirst
+  var input = ' ';
+  while (!rs.eof) {
+    input += rs.fields(0) + "\n";
+    rs.movenext;
+  }
+  alert(input);
+  rs.close;
+  Connection.close;
+};
